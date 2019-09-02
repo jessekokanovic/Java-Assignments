5426f72c8d4fca6aba538481937e77ad7913db80
User <User@BigRigforBigBoy>
2019-09-02 18:45:47 +1000

    Add variable demo

--- Practice/src/VariableDemo.java
+++ Practice/src/VariableDemo.java
@@ -5,7 +5,12 @@ public class VariableDemo {
 		
 		int myAge; //declaration
 		myAge = 69; //initialisation
-		System.out.print(myAge); //usage
+		System.out.println(myAge); //usage
+		myAge= 420; // destructive assignment
+		System.out.println(myAge); 
+		myAge = myAge + 1; // relative assignment, increasing value by one
+		System.out.println(myAge); 
 	}
+	
 
 }
