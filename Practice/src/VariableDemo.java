26460f8244a0d3269d4af12ddc6b7a72b7d12ee4
User <User@BigRigforBigBoy>
2019-09-02 18:43:09 +1000

    Add demo for hello world and variables.

---
+++ Practice/src/VariableDemo.java
@@ -0,0 +1,11 @@
+
+public class VariableDemo {
+
+	public static void main(String[] args) {
+		
+		int myAge; //declaration
+		myAge = 69; //initialisation
+		System.out.print(myAge); //usage
+	}
+
+}
