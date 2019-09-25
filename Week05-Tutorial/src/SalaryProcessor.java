d17f1dc011d89655a99f39d8bfc5230f3e16650d
User <User@BigRigforBigBoy>
2019-09-25 21:15:36 +1000

    Complete up to 5.2

--- Week05-Tutorial/src/SalaryProcessor.java
+++ Week05-Tutorial/src/SalaryProcessor.java
@@ -1,19 +1,31 @@
+import javax.swing.JOptionPane;
 
 public class SalaryProcessor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		double[] salaries; // Step 1: Declare name and type of array and its elements
-		salaries = new double[3]; // Step 2: Create and assign compatible array with 3 elements
-		salaries[0] = 80000.0; // Assign value to first element
-		salaries[1] = 100000.0; // Assign value to second element
-		salaries[2] = 70000.0; // Assign value to third (last) element
+		String[] jobs;
+		int arraySize = Integer.parseInt(JOptionPane.showInputDialog("How many salaries?"));
+		while(arraySize < 0) {
+			arraySize = Integer.parseInt(JOptionPane.showInputDialog("Please enter a postive integer:"));
+		}
+		
+		salaries = new double[arraySize]; // Step 2: Create and assign compatible array with 3 elements
+		jobs = new  String[arraySize];
+		int i = 0;
+		while(i < arraySize) {
+			jobs[i] = JOptionPane.showInputDialog("Enter the Job:");
+			salaries[i] = Double.parseDouble(JOptionPane.showInputDialog("Enter the salary:"));
+			i++;
+		}
+		
 		
 		// Loop goes through indices 0, 1, 2
-		int i = 0;
-		while (i < 3) {
+		i = 0;
+		while (i < arraySize) {
 		// Retrieve and display value at i (current element)
-			System.out.println("Salary at element " + i + " is $" + salaries[i]);
+			System.out.println("Salary for job " + jobs[i] + " is $" + salaries[i]);
 			i = i + 1;
 		}
 	}
