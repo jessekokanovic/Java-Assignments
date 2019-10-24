05c8255c16f284851ffc10ca6ec7173a788d7964
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2019-10-24 15:33:27 +1000

    start constructor for bookcollection

--- Assignment3/src/BookCollection.java
+++ Assignment3/src/BookCollection.java
@@ -1,9 +1,18 @@
+import javax.swing.JOptionPane;
 
 public class BookCollection {
-
+	
+	Book[] bookCollection; //initialize the array to hold the book objects.
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	}
+	
+	public BookCollection(){
+		//A collection of Book objects stored in an array.
+		
+		//we need to set the length of our book array by reading how many lines are in the file and assigning the count as the length. 
+		//this.bookCollection = new Book[];
 	}
 
 }
