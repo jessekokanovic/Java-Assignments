eb43091d93c13fa358b9cc2aa520eb727cda7d15
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2019-10-24 15:06:39 +1000

    Add book class with methods

---
+++ Assignment3/src/Book.java
@@ -0,0 +1,42 @@
+
+public class Book {
+
+	private String title;
+	private String author;
+	private int pages;
+	private int pagesRead;
+	
+	public Book(String title, String author, int pages, int pagesRead) {
+		//Assign the parameters entered by the user to our member variables so we can work with them in our methods.
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.pagesRead = pagesRead;
+	}
+	
+	public String getTitle() {
+		//Get the title of a book and return it as a string
+		return this.getTitle();
+	}
+	
+	public String getAuthor() {
+		//Get the author of a book and return it as a string
+		return this.getAuthor();
+	}
+	
+	public int getPages(){
+		//Get the total number of pages of a book and return it as an integer
+		return this.pages;
+	}
+	
+	public int getPagesRead() {
+		//Get the number of pages of a book that have been read and return it as an integer
+		return this.pagesRead;
+	}
+	
+	public void setPagesRead(int newPagesRead) {
+		//Update the number of pages read for a book
+		this.pagesRead = newPagesRead;
+	}
+	
+}
