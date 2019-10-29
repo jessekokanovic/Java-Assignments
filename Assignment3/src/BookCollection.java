67daf4be6249d11eab7d1f538467fddca46115bf
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2019-10-29 12:12:40 +1000

    Finish 1st attempt at loading books.

--- Assignment3/src/BookCollection.java
+++ Assignment3/src/BookCollection.java
@@ -15,7 +15,7 @@ public class BookCollection {
 
 		this.collectionLength = getCollectionLength(); // run the getCollectionLength method to get the initial number
 														// of books in the collection
-
+		this.bookCollection = loadBooks();
 	}
 
 	public int getCollectionLength() {
@@ -40,20 +40,36 @@ public class BookCollection {
 	}
 
 	public Book[] loadBooks() {
+		// Read each line in the csv and split the string by the delimiter. Use this
+		// information to create a new book object that is added to a list of book
+		// objects. Return this book object so that it may be used.
 
+		Book[] loadedCollection = new Book[this.collectionLength];
+		int lineNum = 0;
 		BufferedReader inFile = null;
 		try {
 			inFile = new BufferedReader(new FileReader("books.csv"));
 			String currentLine = inFile.readLine();
 			while (currentLine != null) {
-				
-				
+				String[] bookInfo = currentLine.split(","); // use the comma as the delimiter in the csv file to get
+															// each individual bit of info for the book
+				Book newBook = new Book(bookInfo[0], bookInfo[1], Integer.parseInt(bookInfo[2]),
+						Integer.parseInt(bookInfo[3]));
+				loadedCollection[lineNum] = newBook;
 				currentLine = inFile.readLine();
 			}
 			inFile.close();
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+		return loadedCollection;
+	}
+
+	public void showBooks() {
+		// Loop over the collection array and format the book information into a string
+		// readable by the user.
+		String collection;
+
 	}
 
 }
--- Practice/src/Greeter.java
+++ Practice/src/Greeter.java
@@ -3,7 +3,9 @@ public class Greeter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Whattup earth");
+		String a = "a,b,3";
+		String[] b = a.split(",");
+		System.out.print(b[0] + b[1]);
 	}
 
 }
