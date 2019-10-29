da3f15c01c56b96ecb467310b4fc8ee1108a7e1e
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2019-10-29 13:05:51 +1000

    Add display books method

--- Assignment3/src/BookCollection.java
+++ Assignment3/src/BookCollection.java
@@ -5,17 +5,25 @@ public class BookCollection {
 
 	private Book[] bookCollection; // initialize the array to hold the book objects.
 	private int collectionLength;
+	private String filePath;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		BookCollection collection = new BookCollection();
 	}
 
 	public BookCollection() {
 		// A collection of Book objects stored in an array.
 
+		this.filePath = "C:/Users/User/git/Java-Assignments/Assignment3/src/books.txt"; // the file path of the stored
+																						// books, change here to match
+																						// where it is saved.
 		this.collectionLength = getCollectionLength(); // run the getCollectionLength method to get the initial number
 														// of books in the collection
 		this.bookCollection = loadBooks();
+
+		// Display the collection
+		displayCollection();
 	}
 
 	public int getCollectionLength() {
@@ -26,7 +34,7 @@ public class BookCollection {
 		int lineNum = 0;
 		BufferedReader inFile = null;
 		try {
-			inFile = new BufferedReader(new FileReader("books.csv"));
+			inFile = new BufferedReader(new FileReader(this.filePath));
 			String currentLine = inFile.readLine();
 			while (currentLine != null) {
 				lineNum++;
@@ -48,7 +56,7 @@ public class BookCollection {
 		int lineNum = 0;
 		BufferedReader inFile = null;
 		try {
-			inFile = new BufferedReader(new FileReader("books.csv"));
+			inFile = new BufferedReader(new FileReader(this.filePath));
 			String currentLine = inFile.readLine();
 			while (currentLine != null) {
 				String[] bookInfo = currentLine.split(","); // use the comma as the delimiter in the csv file to get
@@ -56,6 +64,7 @@ public class BookCollection {
 				Book newBook = new Book(bookInfo[0], bookInfo[1], Integer.parseInt(bookInfo[2]),
 						Integer.parseInt(bookInfo[3]));
 				loadedCollection[lineNum] = newBook;
+				lineNum++;
 				currentLine = inFile.readLine();
 			}
 			inFile.close();
@@ -65,10 +74,21 @@ public class BookCollection {
 		return loadedCollection;
 	}
 
-	public void showBooks() {
+	public void displayCollection() {
 		// Loop over the collection array and format the book information into a string
 		// readable by the user.
-		String collection;
+
+		String collection = "";
+		int i = 0;
+		while (i < this.collectionLength) {
+			collection = collection + this.bookCollection[i].getTitle() + " by " + this.bookCollection[i].getAuthor()
+					+ ", " + this.bookCollection[i].getPagesRead() + " pages read out of  " + this.bookCollection[i].getPagesRead()
+					+ " pages.\n";
+			i++;
+		}
+
+		JOptionPane.showMessageDialog(null,
+				"Books in Collection: " + this.collectionLength + "\n" + collection);
 
 	}
 
