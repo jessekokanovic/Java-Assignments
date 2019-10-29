c533fcdbb65c99637181b654e76316743bcab715
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2019-10-29 11:51:14 +1000

    Begin file loading.

--- Assignment3/src/BookCollection.java
+++ Assignment3/src/BookCollection.java
@@ -1,18 +1,59 @@
 import javax.swing.JOptionPane;
+import java.io.*;
 
 public class BookCollection {
-	
-	Book[] bookCollection; //initialize the array to hold the book objects.
-	
+
+	private Book[] bookCollection; // initialize the array to hold the book objects.
+	private int collectionLength;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	}
-	
-	public BookCollection(){
-		//A collection of Book objects stored in an array.
-		
-		//we need to set the length of our book array by reading how many lines are in the file and assigning the count as the length. 
-		//this.bookCollection = new Book[];
+
+	public BookCollection() {
+		// A collection of Book objects stored in an array.
+
+		this.collectionLength = getCollectionLength(); // run the getCollectionLength method to get the initial number
+														// of books in the collection
+
+	}
+
+	public int getCollectionLength() {
+		// Count the number of lines in the books csv file to determine how many books
+		// in the collection there are.
+		// Do this by reading each line of the file, and adding 1 to a counter for each
+		// book read.
+		int lineNum = 0;
+		BufferedReader inFile = null;
+		try {
+			inFile = new BufferedReader(new FileReader("books.csv"));
+			String currentLine = inFile.readLine();
+			while (currentLine != null) {
+				lineNum++;
+				currentLine = inFile.readLine();
+			}
+			inFile.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return lineNum;
+	}
+
+	public Book[] loadBooks() {
+
+		BufferedReader inFile = null;
+		try {
+			inFile = new BufferedReader(new FileReader("books.csv"));
+			String currentLine = inFile.readLine();
+			while (currentLine != null) {
+				
+				
+				currentLine = inFile.readLine();
+			}
+			inFile.close();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 }
