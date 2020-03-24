4b6f3b5478565b3921f29e2987256923999b2557
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2020-03-24 23:20:41 +1000

    variables

--- Prog1Assignment1/src/JourneyBookingSystem.java
+++ Prog1Assignment1/src/JourneyBookingSystem.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
 
 public class JourneyBookingSystem {
 	
 	public static void main(String[] args) {
 		//Books seats on the train carriages and produces a receipt.
 		
-		int first_seats = 45;//
+		//Define pricing as constant integers 
+		int FIRST_ADULT = 80;
+		int STANDARD_ADULT = 55;
+		int STANDARD_CHILD = 30;
+		int EXCURSION_ADULT = 40;
+		int EXCURSION_CHILD = 20;
+		
+		//Create initial values for available seats and number of passengers
+		int firstSeats = 48;
+		int standardSeats = 70;
+		int excursionSeats = 95;
+		int numAdults;
+		int numChildren;
+		int youngChildren;
+		
+		Scanner console = new Scanner(System.in);
 
 
 	}
