679b89c936a81e99a7a9951ddef7305953e3b281
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2020-03-24 23:11:57 +1000

    yaya

--- Prog1Assignment1/src/JourneyBookingSystem.java
+++ Prog1Assignment1/src/JourneyBookingSystem.java
@@ -4,7 +4,7 @@ public class JourneyBookingSystem {
 	public static void main(String[] args) {
 		//Books seats on the train carriages and produces a receipt.
 		
-		int first_seats = 45;
+		int first_seats = 45;//
 
 
 	}
