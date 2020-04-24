69f23fd0616905210220e2445736db662c55f32c
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2020-04-24 12:16:58 +1000

    add cost calculations

--- Prog1Assignment2/src/Demonstration.java
+++ Prog1Assignment2/src/Demonstration.java
@@ -7,6 +7,7 @@ public class Demonstration {
 	private int maxAttendence;
 	private int startTime;
 	private int duration;
+	//private int[] attendees;
 
 	public Demonstration(String id, String title, double baseFee, int maxAttendence, int startTime, int duration) {
 		//Assign parameters to class variables in constructor.
@@ -16,6 +17,7 @@ public class Demonstration {
 		this.maxAttendence = maxAttendence;
 		this.startTime = startTime;
 		this.duration = duration;
+		//this.attendees = new int[maxAttendence];
 		
 	}
 	
@@ -27,11 +29,30 @@ public class Demonstration {
 		System.out.printf("%-20s%-20s$%-20.2f\n", "", "BASE FEE:",this.baseFee);
 		System.out.printf("%-20s%-20s%-20s\n", "", "MAX ATTENDENCE:", this.maxAttendence);
 		System.out.printf("%-20s%-20s%-20s\n", "", "START TIME:", this.startTime + " (24 Hour Time)");
-		System.out.printf("%-20s%-20s%-20s\n", "", "DURATION:", this.duration + " Minutes");
-
-
-		
+		System.out.printf("%-20s%-20s%-20s\n", "", "DURATION:", this.duration + " Minutes");		
 	}
 	
+	public void calculateCost(int concession, int frs, int arhs) {
+		//Calculate the correct cost based on what discount the reservation qualifies for, 0 for no 1 for yes
+		if(arhs == 1) {
+			//Discount by 25%
+			System.out.println("Cost: $" + this.baseFee* 0.75);
+		}
+		else if(frs == 1){
+			//discount 20%
+			System.out.println("Cost: $" + this.baseFee* 0.80);
+		}
+		else if(concession == 1) {
+			//discount 10%
+			System.out.println("Cost: $" + this.baseFee* 0.90);
+		}
+		else {
+			System.out.println(this.baseFee);
+		}
+		
+		
+		
+		
+	}
 
 }
