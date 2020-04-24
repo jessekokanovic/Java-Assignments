f6c736339e137229218860008d67e75c739ad3ee
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2020-04-24 11:45:53 +1000

    add details method

--- Prog1Assignment2/src/Demonstration.java
+++ Prog1Assignment2/src/Demonstration.java
@@ -1,9 +1,37 @@
 
 public class Demonstration {
+	//Create a demonstration event	
+	private String id;
+	private String title;
+	private double baseFee;
+	private int maxAttendence;
+	private int startTime;
+	private int duration;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Demonstration(String id, String title, double baseFee, int maxAttendence, int startTime, int duration) {
+		//Assign parameters to class variables in constructor.
+		this.id = id;
+		this.title = title;
+		this.baseFee = baseFee;
+		this.maxAttendence = maxAttendence;
+		this.startTime = startTime;
+		this.duration = duration;
+		
 	}
+	
+	public void displayDetails(){
+		//Display the event details
+		System.out.printf("%-10s%-10s%-10s\n","", this.title, "");
+		System.out.println("*********************************************************************");
+		System.out.printf("%-20s%-20s%-20s\n", "", "EVENT ID:", this.id);
+		System.out.printf("%-20s%-20s$%-20.2f\n", "", "BASE FEE:",this.baseFee);
+		System.out.printf("%-20s%-20s%-20s\n", "", "MAX ATTENDENCE:", this.maxAttendence);
+		System.out.printf("%-20s%-20s%-20s\n", "", "START TIME:", this.startTime + " (24 Hour Time)");
+		System.out.printf("%-20s%-20s%-20s\n", "", "DURATION:", this.duration + " Minutes");
+
+
+		
+	}
+	
 
 }
--- Prog1Assignment2/src/TestHarness.java
+++ Prog1Assignment2/src/TestHarness.java
@@ -4,6 +4,10 @@ public class TestHarness {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Demonstration d123 = new Demonstration("d123", "Inspect and prepare a heritage steam locomotive", 100.00,
+				3, 10, 180);
+		d123.displayDetails();
+		
 	}
 
 }
