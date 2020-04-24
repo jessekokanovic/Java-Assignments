422c5d6a150adf90546430046c079dd51c287488
jessekokanovic <41411258+jessekokanovic@users.noreply.github.com>
2020-04-24 12:27:49 +1000

    add calculating cost and tests

--- Prog1Assignment2/src/Demonstration.java
+++ Prog1Assignment2/src/Demonstration.java
@@ -4,20 +4,20 @@ public class Demonstration {
 	private String id;
 	private String title;
 	private double baseFee;
-	private int maxAttendence;
+	private int maxAttendance;
 	private int startTime;
 	private int duration;
 	//private int[] attendees;
 
-	public Demonstration(String id, String title, double baseFee, int maxAttendence, int startTime, int duration) {
+	public Demonstration(String id, String title, double baseFee, int maxAttendance, int startTime, int duration) {
 		//Assign parameters to class variables in constructor.
 		this.id = id;
 		this.title = title;
 		this.baseFee = baseFee;
-		this.maxAttendence = maxAttendence;
+		this.maxAttendance = maxAttendance;
 		this.startTime = startTime;
 		this.duration = duration;
-		//this.attendees = new int[maxAttendence];
+		//this.attendees = new int[maxAttendance];
 		
 	}
 	
@@ -27,14 +27,18 @@ public class Demonstration {
 		System.out.println("*********************************************************************");
 		System.out.printf("%-20s%-20s%-20s\n", "", "EVENT ID:", this.id);
 		System.out.printf("%-20s%-20s$%-20.2f\n", "", "BASE FEE:",this.baseFee);
-		System.out.printf("%-20s%-20s%-20s\n", "", "MAX ATTENDENCE:", this.maxAttendence);
+		System.out.printf("%-20s%-20s%-20s\n", "", "MAX ATTENDANCE:", this.maxAttendance);
 		System.out.printf("%-20s%-20s%-20s\n", "", "START TIME:", this.startTime + " (24 Hour Time)");
 		System.out.printf("%-20s%-20s%-20s\n", "", "DURATION:", this.duration + " Minutes");		
 	}
 	
-	public void calculateCost(int concession, int frs, int arhs) {
+	public void calculateCost(int concession, int frs, int arhs, int mhr) {
 		//Calculate the correct cost based on what discount the reservation qualifies for, 0 for no 1 for yes
-		if(arhs == 1) {
+		if(mhr == 1) {
+			//free attendance
+			System.out.println("Cost: $0");
+		}
+		else if(arhs == 1) {
 			//Discount by 25%
 			System.out.println("Cost: $" + this.baseFee* 0.75);
 		}
@@ -47,7 +51,7 @@ public class Demonstration {
 			System.out.println("Cost: $" + this.baseFee* 0.90);
 		}
 		else {
-			System.out.println(this.baseFee);
+			System.out.println("Cost: $" + this.baseFee);
 		}
 		
 		
--- Prog1Assignment2/src/TestHarness.java
+++ Prog1Assignment2/src/TestHarness.java
@@ -6,7 +6,25 @@ public class TestHarness {
 
 		Demonstration d123 = new Demonstration("d123", "Inspect and prepare a heritage steam locomotive", 100.00,
 				3, 10, 180);
+		
+		//test displaying details
 		d123.displayDetails();
+		
+		//test discount application
+		//no discount
+		d123.calculateCost(0, 0, 0, 0);
+		//concession want 90
+		d123.calculateCost(1, 0, 0, 0);
+		//frs want 80
+		d123.calculateCost(0, 1, 0, 0);
+		//arhs want 75
+		d123.calculateCost(0, 0, 1, 0);
+		//mhr want 0
+		d123.calculateCost(0, 0, 0, 1);
+		//all memberships want 0
+		d123.calculateCost(1, 1, 1, 1);
+		
+		
 		
 	}
 
